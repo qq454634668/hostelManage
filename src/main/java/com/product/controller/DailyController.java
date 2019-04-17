51207aa4d531d22e3a88f9c6c9538bc53475b3ac
wunan <454634668@qq.com>
2019-04-17 16:13:17 +0800

    日常管理文件修改

--- src/main/java/com/product/controller/DailyController.java
+++ src/main/java/com/product/controller/DailyController.java
@@ -22,30 +22,6 @@ public class DailyController {
 
 
 
-    @RequestMapping("/example")
-    @ResponseBody
-    public Map<String,Object> example(HttpServletRequest request){
-        Map<String,Object> result = new HashMap<>();
-        Map<String,Object> param = new HashMap<>();
-        try{
-            result.put("message","调用成功");
-            result.put("code","200");
-            result.put("data",null);
-//            List<Map<String,Object>> list=new ArrayList<>();
-//            PageInfo<Map<String,Object>> pageList = new PageInfo<>(list);
-//            result.put("data",pageList);
-
-//            Date date = new Date();   时间创建
-//            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-//            String created_time = sdf.format(date);
-        }catch (Exception e){
-            result.put("message","调用失败");
-            result.put("code","500");
-            result.put("data",e.getMessage());
-        }
-        return result;
-    }
-
     /**--------------------------------菜单 start----------------------------------------*/
 
 
