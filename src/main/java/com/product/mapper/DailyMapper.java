ec687041419041d06f644cccf70115c4f8408d58
wunan <454634668@qq.com>
2019-04-18 17:13:12 +0800

    床位按楼的一个根据fjbh分组的list

--- src/main/java/com/product/mapper/DailyMapper.java
+++ src/main/java/com/product/mapper/DailyMapper.java
@@ -4,6 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 public interface DailyMapper {
-
+    /**
+     * 按房间编号分组  查询条件到楼，因为展示也只能展示到楼
+     */
+    List<Map<String,Object>> FjbhList(Map<String,Object> param);
+    /**
+     *
+     */
+    List<Map<String,Object>> BedList(Map<String,Object> param);
 
 }
