42d605ae25b1b80ae7eb038c20b90b55dbe8dc16
hmini <15357082399@163.com>
2019-07-05 10:54:26 +0800

    菜

--- src/vue/src/script/occupancyManage.js
+++ src/vue/src/script/occupancyManage.js
@@ -1,134 +1,132 @@
-import { BedListMap,AllBuildDictionary,ApartDictionary,CampusDictionary,BuildDictionary} from '@/api';
+import { BedListMap, AllBuildDictionary, ApartDictionary, CampusDictionary, BuildDictionary } from '@/api';
 export default {
-    data(){
-      return{
-          tableDataList:[],
-          peopleDataList:[],
-          loading:false,
-          page:{
-             pageNum:1, 
-             pageSize:5,
-             startRow:0,
-             endRow:0
-          },
-          form:{
-            apart:'',
-            apartSelect:'',
-            district:'',
-            districtSelect:'',
-            building:'',
-            buildingSelect:'',
-            buildName:'',
-          }
-        
-          
-      }  
+  data() {
+    return {
+      tableDataList: [],
+      peopleDataList: [],
+      loading: false,
+      page: {
+        pageNum: 1,
+        pageSize: 5,
+        startRow: 0,
+        endRow: 0
+      },
+      form: {
+        apart: '',
+        apartSelect: '',
+        district: '',
+        districtSelect: '',
+        building: '',
+        buildingSelect: '',
+        buildName: '',
+      }
+
+
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.tableData();
     },
-    methods:{
-      onSubmit(){
-        this.tableData();
-      },
-      //下拉框选中事件
-        selectGet(vId){//这个vId也就是value值
-          let obj = {};
-          obj = this.form.buildingSelect.find((item)=>{//这里的userList就是上面遍历的数据源
-              return item.code === vId;//筛选出匹配数据
-          });
-          this.form.buildName = obj.name;
-        },
-      selectChange(apart){
-           if(apart == 'apart'){
-               this.form.district="";
-               this.form.districtSelect = [];
-               this.form.building="";
-               this.form.buildingSelect = [];
-               this.districtPart(this.form.apart);
-               
-           }else if(apart == "district"){
-                this.form.building="";
-                this.apartLou(this.form.district)
-           }
-      },
-      link(){
-        this.$router.push({ path: '/xzz' });
-      },
-      // 床位
-      async bedList(){
-          this.loading = true;
-           var params = {
-            xqbh:this.form.apart,
-            gybh:this.form.district,
-            loubh:this.form.building
-           };
-          var res = await BedListMap(params);
-          if(res.code == 200){
-              this.loading = false;
-              this.tableDataList = res.data;
-                console.log(res)
-          }else{
-            this.loading = false;
-            this.$message(res.message)
-          }
+    //下拉框选中事件
+    selectGet(vId) {//这个vId也就是value值
+      let obj = {};
+      obj = this.form.buildingSelect.find((item) => {//这里的userList就是上面遍历的数据源
+        return item.code === vId;//筛选出匹配数据
+      });
+      this.form.buildName = obj.name;
+    },
+    selectChange(apart) {
+      if (apart == 'apart') {
+        this.form.district = "";
+        this.form.districtSelect = [];
+        this.form.building = "";
+        this.form.buildingSelect = [];
+        this.districtPart(this.form.apart);
+
+      } else if (apart == "district") {
+        this.form.building = "";
+        this.apartLou(this.form.district)
+      }
+    },
+    link() {
+      this.$router.push({ path: '/xzz' });
+    },
+    // 床位
+    async bedList() {
+      this.loading = true;
+      var params = {
+        xqbh: this.form.apart,
+        gybh: this.form.district,
+        loubh: this.form.building
+      };
+      var res = await BedListMap(params);
+      if (res.code == 200) {
+        this.loading = false;
+        this.tableDataList = res.data;
+        console.log(res)
+      } else {
+        this.loading = false;
+        this.$message(res.message)
+      }
     },
 
-     // 校区
-      async apart(){
-            var params = {};
-            var res = await CampusDictionary(params);
-            if(res.code == 200){
-               this.form.apartSelect = res.data;
-            }else{
-              this.$message(res.message)
-            }
-      },
-       // 公寓区下拉
-       async districtPart(code){
-            var params = {
-              code:code
-            };
-            var res = await ApartDictionary(params);
-            if(res.code == 200){
-              this.loading = false;
-              this.form.districtSelect = res.data; 
-            }else{
-              this.loading = false;
-              this.$message(res.message);
-              return 
-            }
-      },
-         // 公寓楼
-      async apartLou(code){
-            var params = {
-                code:code
-            };
-            var res = await BuildDictionary(params);
-            if(res.code == 200){
-              this.form.buildingSelect = res.data;
-            }else{
-              this.$message(res.message)
-            }
-      },
-        // 公寓楼all
-        async apartLouAll(){
-            var params = {
-                code:''
-            };
-            var res = await AllBuildDictionary(params);
-            if(res.code == 200){
-              this.form.buildingSelect = res.data;
-              this.form.building = res.data[0].code;
-              this.form.buildName = res.data[0].name;
-              this.bedList();
-            }else{
-              this.$message(res.message)
-            }
-      },
-  
+    // 校区
+    async apart() {
+      var params = {};
+      var res = await CampusDictionary(params);
+      if (res.code == 200) {
+        this.form.apartSelect = res.data;
+      } else {
+        this.$message(res.message)
+      }
     },
-    mounted(){
-    //    this.tableData();
-    //    this.hfgz('allocation');
-        this.apartLouAll()
-        this.apart();
-    }
+    // 公寓区下拉
+    async districtPart(code) {
+      var params = {
+        code: code
+      };
+      var res = await ApartDictionary(params);
+      if (res.code == 200) {
+        this.loading = false;
+        this.form.districtSelect = res.data;
+      } else {
+        this.loading = false;
+        this.$message(res.message);
+        return
+      }
+    },
+    // 公寓楼
+    async apartLou(code) {
+      var params = {
+        code: code
+      };
+      var res = await BuildDictionary(params);
+      if (res.code == 200) {
+        this.form.buildingSelect = res.data;
+      } else {
+        this.$message(res.message)
+      }
+    },
+    // 公寓楼all
+    async apartLouAll() {
+      var params = {
+        code: ''
+      };
+      var res = await AllBuildDictionary(params);
+      if (res.code == 200) {
+        this.form.buildingSelect = res.data;
+        this.form.building = res.data[0].code;
+        this.form.buildName = res.data[0].name;
+        this.bedList();
+      } else {
+        this.$message(res.message)
+      }
+    },
+
+  },
+  mounted() {
+    this.apartLouAll()
+    this.apart();
+  }
 }
--- src/vue/src/views/dailyManage/xzz.vue
+++ src/vue/src/views/dailyManage/xzz.vue
@@ -5,108 +5,55 @@
           <div class="content">
             <el-container>
                 <el-header>
-                  <el-form :inline="true" :model="form" class="demo-form-inline left">
-                       <el-form-item label="">
-                         <el-select v-model="form.apart" placeholder="校区" @change="selectChange('apart')">
-                           <el-option v-for="(item,index) in form.apartSelect" :key="index" :label="item.name" :value="item.code"></el-option>
-                        </el-select>
-                      </el-form-item>
-                      <el-form-item label="">
-                         <el-select v-model="form.district" placeholder="公寓区" @change="selectChange('district')">
-                           <el-option v-for="(item,index) in form.districtSelect" :key="index" :label="item.name" :value="item.code"></el-option>
-                        </el-select>
-                      </el-form-item>
-                      <el-form-item label="">
-                         <el-select v-model="form.building" placeholder="选择公寓楼" @change="selectGet" filterable>
-                           <el-option v-for="(item,index) in form.buildingSelect" :key="index" :label="item.name" :value="item.code"></el-option>
-                        </el-select>
-                      </el-form-item>
-                      <el-form-item>
+                      <el-form :inline="true" :model="form" class="demo-form-inline left">
+                        <el-form-item label>
+                            <el-select v-model="form.apart" placeholder="校区" @change="selectChange('apart')">
+                                <el-option
+                                v-for="(item,index) in form.apartSelect"
+                                :key="index"
+                                :label="item.name"
+                                :value="item.code"
+                                ></el-option>
+                            </el-select>
+                            </el-form-item>
+                            <el-form-item label>
+                            <el-select
+                                v-model="form.district"
+                                placeholder="公寓区"
+                                @change="selectChange('district')"
+                            >
+                                <el-option
+                                v-for="(item,index) in form.districtSelect"
+                                :key="index"
+                                :label="item.name"
+                                :value="item.code"
+                                ></el-option>
+                            </el-select>
+                            </el-form-item>
+                            <el-form-item label>
+                            <el-select
+                                v-model="form.building"
+                                placeholder="选择公寓楼"
+                                @change="selectGet"
+                                filterable
+                            >
+                                <el-option
+                                v-for="(item,index) in form.buildingSelect"
+                                :key="index"
+                                :label="item.name"
+                                :value="item.code"
+                                ></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item>
                         <el-button type="primary" @click="bedList()">查询</el-button>
-                      </el-form-item>
-                  </el-form>
+                        </el-form-item>
+                    </el-form>
                 </el-header>
-                <el-main class="border padding-0">
-                        <div class="flex-box">
-                             <div class="onewrap" v-for="(item,index) in tableDataList" :key="index">
-                                <div class="buildShow">
-                                  <!-- <span>{{form.buildName}}</span> -->
-                                  <span>房间编号:{{item.fjbh}}</span>
-                                </div>
-                                <div class="flex-box border-right zynr">
-                                      <div v-for="(tem,index2) in item.fjList" :key="index2">
-                                          <div v-show="tem.zt==2"><span class="xuhao">{{index2+1}}</span>{{tem.realname}}</div>
-                                          <div v-show="tem.zt==1" class="xianzhi" @click="link()"><span class="xuhao">{{index2+1}}</span>{{tem.ztname}}</div>
-                                      </div>
-                                      
-                               </div>
-                            </div>
-                             <div class="onewrap" v-for="(item,index) in tableDataList" :key="index">
-                                <div class="buildShow">
-                                  <!-- <span>{{form.buildName}}</span> -->
-                                  <span>房间编号:{{item.fjbh}}</span>
-                                </div>
-                                <div class="flex-box border-right zynr">
-                                      <div v-for="(tem,index2) in item.fjList" :key="index2">
-                                          <div v-show="tem.zt==2"><span class="xuhao">{{index2+1}}</span>{{tem.realname}}</div>
-                                          <div v-show="tem.zt==1" class="xianzhi" @click="link()"><span class="xuhao">{{index2+1}}</span>{{tem.ztname}}</div>
-                                      </div>
-                                      
-                               </div>
-                            </div>
-                             <div class="onewrap" v-for="(item,index) in tableDataList" :key="index">
-                                <div class="buildShow">
-                                  <!-- <span>{{form.buildName}}</span> -->
-                                  <span>房间编号:{{item.fjbh}}</span>
-                                </div>
-                                <div class="flex-box border-right zynr">
-                                      <div v-for="(tem,index2) in item.fjList" :key="index2">
-                                          <div v-show="tem.zt==2"><span class="xuhao">{{index2+1}}</span>{{tem.realname}}</div>
-                                          <div v-show="tem.zt==1" class="xianzhi"><span class="xuhao">{{index2+1}}</span>{{tem.ztname}}</div>
-                                      </div>
-                                      
-                               </div>
-                            </div>
-                             <div class="onewrap" v-for="(item,index) in tableDataList" :key="index">
-                                <div class="buildShow">
-                                  <!-- <span>{{form.buildName}}</span> -->
-                                  <span>房间编号:{{item.fjbh}}</span>
-                                </div>
-                                <div class="flex-box border-right zynr">
-                                      <div v-for="(tem,index2) in item.fjList" :key="index2">
-                                          <div v-show="tem.zt==2"><span class="xuhao">{{index2+1}}</span>{{tem.realname}}</div>
-                                          <div v-show="tem.zt==1" class="xianzhi"><span class="xuhao">{{index2+1}}</span>{{tem.ztname}}</div>
-                                      </div>
-                                      
-                               </div>
-                            </div>
-                            <div class="onewrap" v-for="(item,index) in tableDataList" :key="index">
-                                <div class="buildShow">
-                                  <!-- <span>{{form.buildName}}</span> -->
-                                  <span>房间编号:{{item.fjbh}}</span>
-                                </div>
-                                <div class="flex-box border-right zynr">
-                                      <div v-for="(tem,index2) in item.fjList" :key="index2">
-                                          <div v-show="tem.zt==2"><span class="xuhao">{{index2+1}}</span>{{tem.realname}}</div>
-                                          <div v-show="tem.zt==1" class="xianzhi"><span class="xuhao">{{index2+1}}</span>{{tem.ztname}}</div>
-                                      </div>
-                                      
-                               </div>
-                            </div>
-                            <div class="onewrap" v-for="(item,index) in tableDataList" :key="index">
-                                <div class="buildShow">
-                                  <!-- <span>{{form.buildName}}</span> -->
-                                  <span>房间编号:{{item.fjbh}}</span>
-                                </div>
-                                <div class="flex-box border-right zynr">
-                                      <div v-for="(tem,index2) in item.fjList" :key="index2">
-                                          <div v-show="tem.zt==2"><span class="xuhao">{{index2+1}}</span>{{tem.realname}}</div>
-                                          <div v-show="tem.zt==1" class="xianzhi" @click="link()"><span class="xuhao">{{index2+1}}</span>{{tem.ztname}}</div>
-                                      </div>
-                                      
-                               </div>
-                            </div>
-                        </div>
+                <el-main class="padding-0">
+                      <div></div>
+                      <div></div>
+                      <div></div>
                 </el-main>
             </el-container>
           
@@ -117,53 +64,81 @@
 </template>
 <script>
   import { mapMutations } from 'vuex';
+  import { BedListMap, AllBuildDictionary, ApartDictionary, CampusDictionary, BuildDictionary } from '@/api';
   import { jquery } from '@/script/jquery-1.7.1';
-  import occupancyManage from '@/script/occupancyManage.js';
 export default{
-  ...occupancyManage
+  data(){
+      return{
+         form: {
+            apart: '',
+            apartSelect: '',
+            district: '',
+            districtSelect: '',
+            building: '',
+            buildingSelect: '',
+            buildName: ''
+        }  
+      }
+  },
+  methods:{
+          // 校区
+    async apart() {
+      var params = {};
+      var res = await CampusDictionary(params);
+      if (res.code == 200) {
+        this.form.apartSelect = res.data;
+      } else {
+        this.$message(res.message)
+      }
+    },
+    // 公寓区下拉
+    async districtPart(code) {
+      var params = {
+        code: code
+      };
+      var res = await ApartDictionary(params);
+      if (res.code == 200) {
+        this.loading = false;
+        this.form.districtSelect = res.data;
+      } else {
+        this.loading = false;
+        this.$message(res.message);
+        return
+      }
+    },
+    // 公寓楼
+    async apartLou(code) {
+      var params = {
+        code: code
+      };
+      var res = await BuildDictionary(params);
+      if (res.code == 200) {
+        this.form.buildingSelect = res.data;
+      } else {
+        this.$message(res.message)
+      }
+    },
+    // 公寓楼all
+    async apartLouAll() {
+      var params = {
+        code: ''
+      };
+      var res = await AllBuildDictionary(params);
+      if (res.code == 200) {
+        this.form.buildingSelect = res.data;
+      } else {
+        this.$message(res.message)
+      }
+    },
+
+  },
+  mounted(){
+    
+  }
 }
 </script>
-<style lang="stylus" scoped>
-    .page-occupancy{
-      .buildShow{
-            line-height:40px;
-            color:#0000F6;
-           
-            border-bottom:1px solid #dedede;
-        }
-      .flex-box{
-        display: -webkit-flex; 
-        display: flex;
-        flex-wrap: wrap
-        >div{
-          width:200px;
-          // margin-left:20px;
-          // margin-top:20px;
-          
-        }
-        .onewrap{
-           border-top:1px solid #dedede;
-          border-bottom:1px solid #dedede;
-          border-right:1px solid #dedede;
-          margin-top:20px;
-           .zynr{
-             line-height:40px;
-             .xianzhi{
-               color:orange;
-               cursor pointer;
-             }
-             .xuhao{
-               display:inline-block;
-               padding:0 10px;
-             }
-           }
-        }
-        .border-right{
-          // border-right:1px solid #999;
-        }
-        
-      }
-    }
+<style lang="stylus" scoped>      
+      
 </style>
 
 
